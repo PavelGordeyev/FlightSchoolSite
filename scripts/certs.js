ccbdc5332c653a712f9236f7f171926dcd47b509
Pavel Gordeyev <gordeyev.p@gmail.com>
2020-02-28 19:16:06 -0800

    Add certs js with bootstrap scrollbar

---
+++ scripts/certs.js
@@ -0,0 +1,37 @@
+// Certificate class
+var certificate = function(name,desc){
+	this.name = name;
+	this.desc = desc;
+}
+
+// Create certificate elements
+function createCert(name,desc){
+
+	var certificate = document.createElement('article'),
+		row = document.createElement('div'),
+		labelDiv = document.createElement('div'),
+		textDiv = document.createElement('div'),
+		text = document.createElement('p');
+
+	// Set classes
+	certificate.classList.add('container');
+	row.classList.add('row');
+	labelDiv.classList.add('col');
+	labelDiv.classList.add('cert-label');
+	textDiv.classList.add('col-6');
+	textDiv.classList.add('overflow-auto');
+	textDiv.classList.add('cert-text');
+	
+	// Set text
+	labelDiv.innerHTML = name;
+	text.innerHTML = desc;
+
+	// Append all elements
+	textDiv.appendChild(text);
+	row.appendChild(labelDiv);
+	row.appendChild(textDiv);
+	certificate.appendChild(row);
+
+	// Add project element to the page
+	document.getElementById('content').appendChild(certificate);
+}
